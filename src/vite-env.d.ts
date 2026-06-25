2b0f188edcc621f864fe36381c75554fdd946691
Shoretick <emax1@hotmail.com>
2026-06-25 16:19:57 -0300

    first commit1

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,6 @@
-// <reference types="vite/client" />
+/// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_GOOGLE_CLIENT_ID: "38563787451-7kiuqqkkhiqn9rij7tggnbijo0k8a3cc.apps.googleusercontent.com"
-}
+
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
