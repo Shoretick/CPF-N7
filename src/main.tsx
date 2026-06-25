2e0f117afcf646c91761ed0a3ba19f5ca43dbe81
Shoretick <emax1@hotmail.com>
2026-06-25 16:51:17 -0300

    first commit1

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
-
+import './index.css'
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? ''
 
