02e892b11d999cd429f06eaeba6c25c44435adc6
Shoretick <emax1@hotmail.com>
2026-06-25 17:08:59 -0300

    first commit1

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_CLIENT_ID: "238563787451-7kiuqqkkhiqn9rij7tggnbijo0k8a3cc.apps.googleusercontent.com"
+} // <-- Te faltaba cerrar esta llave
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
-
-
