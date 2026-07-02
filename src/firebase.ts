5df899664999af4ee82f5f35c45d6fe6c59fcef8
Shoretick <emax1@hotmail.com>
2026-07-02 16:37:28 -0300

    Auth0 , firebase + google, .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBF32zYXdkj5Fn9Hv3C8Uiv5VddufbL7bA",
-  authDomain: "cpf-n7.firebaseapp.com",
-  projectId: "cpf-n7",
-  storageBucket: "cpf-n7.firebasestorage.app",
-  messagingSenderId: "934541472904",
-  appId: "1:934541472904:web:c3d5751596170d561e9b3d",
-  measurementId: "G-LEDN2FPV27"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializar Firebase y Analytics
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GOOGLE_CLIENT_ID: "934541472904-h0omvotbj45vllg4hct29k7hb9sso7fe.apps.googleusercontent.com"
-} // <-- Te faltaba cerrar esta llave
+  readonly VITE_GOOGLE_CLIENT_ID: string
+  
+} 
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
